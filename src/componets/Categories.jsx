04059ce02dbc78bf1318cc0a8b7b77ed4ce9e59a
grisha193 <grisha.777111@mail.ru>
2025-06-03 01:25:11 +0300

    add items and categories

--- src/componets/Categories.jsx
+++ src/componets/Categories.jsx
@@ -1,46 +1,71 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export class Categories extends Component {
-    constructor(props){
-        super(props)
-        this.state =
-        {
-            categories: 
-            [
-                {
-                key: 'all',
-                name: 'Все'
-                },
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [{ key: 'all', name: 'Все' }],
+      isLoading: false,
+      error: null
+    };
+    this._isMounted = false; // Флаг для проверки монтирования
+  }
 
-                {
-                key: 'chairs',
-                name: 'Стулья'
-                },
+  componentDidMount() {
+    this._isMounted = true;
+    this.fetchCategories();
+  }
 
-                {
-                    key: 'tables',
-                    name: 'Столы'
-                },
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
 
-                {
-                    key: 'sofa',
-                    name: 'Диваны'
-                },
+  fetchCategories = () => {
+    if (this.state.isLoading) return; // Защита от повторных запросов
 
-                {
-                    key: 'lights',
-                    name: 'Свет'
-                },
-            ]
+    this.setState({ isLoading: true });
+
+    fetch('http://localhost:8000/api/categories')
+      .then((res) => {
+        if (!res.ok) throw new Error(res.statusText);
+        return res.json();
+      })
+      .then((data) => {
+        if (this._isMounted) {
+          this.setState({
+            categories: [{ key: 'all', name: 'Все' }, ...data],
+            isLoading: false
+          });
         }
-    }
+      })
+      .catch((err) => {
+        if (this._isMounted) {
+          console.error('Ошибка загрузки категорий:', err);
+          this.setState({ error: err.message, isLoading: false });
+        }
+      });
+  };
+
   render() {
+    const { categories, isLoading, error } = this.state;
+
+    if (error) return <div className="error">Ошибка: {error}</div>;
+    if (isLoading) return <div className="loading">Загрузка...</div>;
+
     return (
-      <div className='categories'>{this.state.categories.map(el => (
-        <div key={el.key}  onClick={() => this.props.chooseCategories(el.key)}>{el.name}</div>
-      ))}</div>
-    )
+      <div className="categories">
+        {categories.map((el) => (
+          <div 
+            key={el.key} 
+            onClick={() => this.props.chooseCategories(el.key)}
+            className="category-item"
+          >
+            {el.name}
+          </div>
+        ))}
+      </div>
+    );
   }
 }
 
-export default Categories
+export default Categories;
--- src/componets/Item.jsx
+++ src/componets/Item.jsx
@@ -4,9 +4,13 @@ export class Item extends Component {
   render() {
     return (
       <div className='item'>
-        <img src={"./img/" + this.props.item.img} alt=''  onClick={() => this.props.onShowItem(this.props.item)}/>
-        <h2>{this.props.item.title}</h2>
-        <p>{this.props.item.decs}</p>
+        <img
+          src={`http://localhost:8000/img/${this.props.item.img}`}
+          alt=''
+          onClick={() => this.props.onShowItem(this.props.item)}
+        />
+        <h2>{this.props.item.name}</h2>
+        <p>{this.props.item.description}</p>
         <b>{this.props.item.price}</b>
         <div className='add-to-cart' onClick={() => this.props.onAdd(this.props.item)}>+</div>
       </div>
@@ -14,4 +18,4 @@ export class Item extends Component {
   }
 }
 
-export default Item
+export default Item;
